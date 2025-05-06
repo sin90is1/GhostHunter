03e93a74a60080019bda27db182d25ddf23c342d
Sina Ghavami Adel <sinaghavamy.sg78@gmail.com>
2025-05-06 18:48:59 +0330

    Set-Up magic wand

---
+++ Assets/Scripts/MagicWand.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicWand : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public OVRInput.RawButton ShootingButton;
+    public LineRenderer LinePrefab;
+    public Transform ShootingPoint;
+    public float MaxLineDistance = 5;
+    public float LineShowTimer = 0.3f;
+    public AudioSource ShootingSource;
+    public AudioClip ShootingAudioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(OVRInput.GetDown(ShootingButton))
+        {
+            Shoot();
+        }
+    }
+
+    public void Shoot()
+    {
+        ShootingSource.PlayOneShot(ShootingAudioClip);
+
+        Ray ray = new Ray(ShootingPoint.position,ShootingPoint.forward);
+        bool bHasHit = Physics.Raycast(ray, out RaycastHit hit, MaxLineDistance, layerMask);
+
+        Vector3 endPoint = Vector3.zero;
+
+        if (bHasHit)
+        {
+            endPoint = hit.point;
+        }
+        else
+        {
+            endPoint = ShootingPoint.position + ShootingPoint.forward * MaxLineDistance;
+        }
+
+        LineRenderer line = Instantiate(LinePrefab);
+        line.positionCount = 2;
+        line.SetPosition(0, ShootingPoint.position); 
+        line.SetPosition(1,endPoint);
+
+        Destroy(line.gameObject, LineShowTimer);
+    }
+}
